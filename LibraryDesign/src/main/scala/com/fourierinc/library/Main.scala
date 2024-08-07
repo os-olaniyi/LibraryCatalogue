230312c5b02a51edb95ab18a49bea19f5eebd57d
os-olaniyi <os.olaniyi@outlook.com>
2024-08-07 13:58:59 +0200

    LIBCAD-0011 adds routes for user and session

--- LibraryDesign/src/main/scala/com/fourierinc/library/Main.scala
+++ LibraryDesign/src/main/scala/com/fourierinc/library/Main.scala
@@ -5,19 +5,22 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directive._
 import akka.stream.ActorMaterializer
 import com.fourierinc.library.routes._
+import com.fourierinc.library.utils.ErrorHandler
 
 import scala.io.StdIn
 
-object Main extends App {
+object Main extends App with ErrorHandler {
   implicit val system = ActorSystem("library-system")
   implicit val materializer = ActorMaterializer
   implicit val executionContext = system.dispatcher
   
   val routes =
-    BookRoutes.route ~ 
-    UserRoutes.route ~
-    SessionRoutes.route ~
-    BorrowRoutes.route
+    handleExceptions(ErrorHandler.exceptionHandler){
+      BookRoutes.route ~
+      UserRoutes.route ~
+      SessionRoutes.route ~
+      BorrowRoutes.route
+    }
     
   val bindingFuture = Http().newServerAt("localhost", 8080).bind(routes)
   
